7178d431c86b1a4eaf2df0f3d1ccfd6a10f1edd0
Rusted2361 <hamzasajid4040@gmail.com>
2023-12-16 01:29:13 +0500

    without delay

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//"time"
 	"github.com/gin-gonic/gin"
 	"media-file-server-go/internal/api"
 	 "media-file-server-go/internal/recursion"
@@ -16,32 +16,31 @@ func main() {
 
 	//////uncomment this function to immediately shut service if cluster id not found
 	// Create a new Goroutine for the heartBeat function
-	//go recursion.HeartBeat()
-	//go recursion.SaveNodeOsDetails(0)
+	go recursion.HeartBeat()
+	go recursion.SaveNodeOsDetails(0)
 
 	router := gin.Default()
 
 	// Register API routes
     api.RegisterRoutes(router)
 	//comment this one once you uncomment below recursion
-	// if err := router.Run(":3009"); err != nil {
-	// 	fmt.Println("Failed to start the server:", err)
-	// }
+	if err := router.Run(":3009"); err != nil {
+		fmt.Println("Failed to start the server:", err)
+	}
 
-	///////////uncomment this function to enable heartbeat function with 5 second delay will not immediately shut down/////////////
-	//Run the Gin server on port 3008 in a Goroutine
-	go func() {
-		if err := router.Run(":3008"); err != nil {
-			fmt.Println("Failed to start the server:", err)
-		}
-	}()
+	//Run the Gin server on port 3009 in a Goroutine
+	// go func() {
+	// 	if err := router.Run(":3009"); err != nil {
+	// 		fmt.Println("Failed to start the server:", err)
+	// 	}
+	// }()
 
-	// Wait for 5 seconds
-	<-time.After(5 * time.Second)
+	// // Wait for 5 seconds
+	// <-time.After(5 * time.Second)
 
-	// Start the heartBeat function after 5 seconds
-	go recursion.HeartBeat()
+	// // Start the heartBeat function after 5 seconds
+	// go recursion.HeartBeat()
 
-	// Block the main Goroutine so that the program doesn't exit
-	select {}
+	// // Block the main Goroutine so that the program doesn't exit
+	// select {}
 }
