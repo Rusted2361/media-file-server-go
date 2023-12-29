2cfc5d9a0ef89cf5c83908f1f3385700f4da450e
Rusted2361 <hamzasajid4040@gmail.com>
2023-12-29 22:09:37 +0500

    all go routines

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,7 +5,7 @@ import (
 	//"time"
 	"github.com/gin-gonic/gin"
 	"media-file-server-go/internal/api"
-	//"media-file-server-go/internal/recursion"
+	"media-file-server-go/internal/recursion"
 	
 )
 
@@ -13,19 +13,16 @@ import (
 		//////////////////////////////////////////////////////
 func main() {
 
-	
-	// Start SaveNodeDetails immediately
-	//go recursion.SaveNodeDetails(0)
-
-	router := gin.Default()
-	// Register API routes
-	api.RegisterRoutes(router)
-
 	// Delay for 10 seconds before starting HeartBeat
 	// time.Sleep(10 * time.Second)
-	// go recursion.HeartBeat()
+	go recursion.HeartBeat()
 
-	//comment this one once you uncomment below recursion
+	// Start SaveNodeDetails immediately
+	go recursion.SaveNodeDetails(0)
+
+	// Register API routes
+	router := gin.Default()
+	api.RegisterRoutes(router)
 	if err := router.Run(":3009"); err != nil {
 		fmt.Println("Failed to start the server:", err)
 	}
