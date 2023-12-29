2ef4fd76a2a95a1bcbdedc5ff41768b20426ab41
Rusted2361 <hamzasajid4040@gmail.com>
2023-12-29 23:59:09 +0500

    added download api

--- internal/api/api.go
+++ internal/api/api.go
@@ -18,6 +18,7 @@ func RegisterRoutes(router *gin.Engine) {
     router.GET("/api/file/view/access-play/:accessKey/:token", playVideo)
 	router.GET("/api/file/view/access/:accessKey", getAccessFile)
 	router.GET("/api/file/view/access/:accessKey/:token", getAccessFile)
+	router.GET("/api/file/download/:accessKey/", downloadFile)
 	router.GET("/api/file/download/:accessKey/:token", downloadFile)
 }
 
@@ -357,7 +358,115 @@ func getAccessFile(c *gin.Context) {
 }
 
 func downloadFile(c *gin.Context) {
-	// Implement the logic for the downloadFile function
-	// ...
-	c.String(http.StatusOK, "File content for download")
+		// Extract access key and token from URL parameters
+		accessKey := c.Param("accessKey")
+		token := c.Param("token")
+	
+		// Verify the access token
+		AccessDataResponse, err := helpers.VerifyAccessToken(accessKey, token)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	
+		//This method uses map interfaces to deal with response data
+		accessData, ok := AccessDataResponse["data"].(map[string]interface{})
+		if ok {
+			 fmt.Println("accessData value is accessed")
+		 }
+		fileMetaDataValue, ok := accessData["fileMetaData"].([]interface{})
+		if ok {
+		 fmt.Println("fileMetaDataValue is a valid array")
+		}
+	
+		// Custom sorting function
+		sort.Slice(fileMetaDataValue, func(i, j int) bool {
+		 indexI, okI := fileMetaDataValue[i].(map[string]interface{})["index"].(float64)
+		 indexJ, okJ := fileMetaDataValue[j].(map[string]interface{})["index"].(float64)
+	
+		// Check if type assertions are successful
+		if okI && okJ {
+			 return int(indexI) < int(indexJ)
+		}
+		// Handle the case where type assertions failed
+			return false
+		})
+	
+		// Storing sorted data in ipfsMetaData
+		ipfsMetaData := fileMetaDataValue
+		fmt.Println("ipfsMetaData sorted")	
+	
+		// Setting response headers for content type and filename
+		c.Writer.Header().Set("Content-Type", accessData["fileType"].(string))
+		c.Writer.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, accessData["fileName"].(string)))
+	
+		// Create a pipe
+		pr, pw := io.Pipe()
+	
+		// Start a goroutine to produce data and write to the pipe
+		go func() {
+			defer pw.Close()
+	
+			// Create an HTTP client with a timeout of 5 seconds
+			client := &http.Client{Timeout: 5 * time.Second}
+	
+			// Looping through ipfsMetaData and fetching file data
+			for i := 0; i < len(ipfsMetaData); i++ {
+				// Type-assert ipfsMetaData[i] to a map[string]interface{}
+				metaData, ok := ipfsMetaData[i].(map[string]interface{})
+				if !ok {
+					// Handle the case where type assertion fails
+					fmt.Println("Error: ipfsMetaData is not a valid map")
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Invalid metadata format"})
+					return
+				}
+	
+				// Fetch the "cid" value from the map
+				cid, ok := metaData["cid"].(string)
+				if !ok {
+					// Handle the case where "cid" key is not present or has an unexpected type
+					fmt.Println("Error: 'cid' key not found or has an unexpected type")
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Invalid CID format"})
+					return
+				}
+	
+				// Making an HTTP GET request to fetch file data from IPFS
+				url := fmt.Sprintf("http://46.101.133.110:8080/api/v0/cat/%s", cid)
+				
+				// Make an HTTP GET request
+				respone, err := client.Get(url)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				defer respone.Body.Close()
+	
+				// Read the response body
+				fileRespone, err := ioutil.ReadAll(respone.Body)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				
+				// Decrypting data using a custom function
+				decryptedData, err := helpers.DecryptedSecretKeyAndFile(
+					accessData["data"].(string), 
+					accessData["secretKey"].(string), 
+					accessData["accessKey"].(string), 
+					accessData["iv"].(string), 
+					accessData["salt"].(string),
+					[]byte(fileRespone),
+				)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				
+				// Write the decrypted data to the pipe
+				pw.Write(decryptedData)
+				
+			}
+		}()
+		// Pipe the reader to the response writer
+		io.Copy(c.Writer, pr)
 }
