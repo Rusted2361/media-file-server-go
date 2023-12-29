11b7b3bddbbfda653f586f677a1641f8bb421be8
Rusted2361 <hamzasajid4040@gmail.com>
2023-12-29 23:22:16 +0500

    added build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build for macOS
+GOOS=darwin GOARCH=amd64 go build -o file-server-mac cmd/main/main.go
+
+# Build for Linux
+GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o file-server-lin cmd/main/main.go
