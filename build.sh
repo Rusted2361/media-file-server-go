f13c506852cdaf7b259a38001f88a82168119c63
Rusted2361 <hamzasajid4040@gmail.com>
2023-12-29 23:35:06 +0500

    added command for win

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ GOOS=darwin GOARCH=amd64 go build -o file-server-mac cmd/main/main.go
 
 # Build for Linux
 GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o file-server-lin cmd/main/main.go
+
+# Build for Windows
+GOOS=windows GOARCH=amd64 go build -o file-server-win.exe cmd/main/main.go
