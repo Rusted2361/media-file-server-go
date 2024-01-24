4848678a99caede68f2ac52e43a973ad763a7e8e
Rusted2361 <hamzasajid4040@gmail.com>
2024-01-24 18:08:25 +0500

    finalized version

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	//"fmt"
-	//"time"
+	"time"
 	"log"
 	"github.com/gin-gonic/gin"
 	"media-file-server-go/internal/api"
-	//"media-file-server-go/internal/recursion"
+	"media-file-server-go/internal/recursion"
 	
 )
 
@@ -14,38 +14,38 @@ import (
 		//////////////////////////////////////////////////////
 func main() {
 	// Delay for 10 seconds before starting HeartBeat
-	//time.Sleep(3 * time.Second)
+	time.Sleep(3 * time.Second)
 
 	// Start SaveNodeDetails immediately
-	//go recursion.SaveNodeDetails(0)
+	go recursion.SaveNodeDetails(0)
 	
 	// Register API routes
 	router := gin.Default()
 	api.RegisterRoutes(router)
 
 	// Run HeartBeat in a goroutine with recovery
-	// go func() {
-	// 	for {
-	// 		func() {
-	// 			defer func() {
-	// 				if r := recover(); r != nil {
-	// 					log.Printf("HeartBeat recovered from panic: %v", r)
-	// 				}
-	// 			}()
+	go func() {
+		for {
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						log.Printf("HeartBeat recovered from panic: %v", r)
+					}
+				}()
 
-	// 			// Start HeartBeat
-	// 			recursion.HeartBeat()
-	// 		}()
+				// Start HeartBeat
+				recursion.HeartBeat()
+			}()
 
-	// 		// Sleep for a short interval before restarting HeartBeat
-	// 		time.Sleep(2 * time.Second)
-	// 	}
-	// }()
+			// Sleep for a short interval before restarting HeartBeat
+			time.Sleep(2 * time.Second)
+		}
+	}()
 
 	// Start video deletion task
-	//go recursion.CleanVideoDirectory("videos")
+	go recursion.CleanVideoDirectory("videos")
 
-	if err := router.Run(":3009"); err != nil {
+	if err := router.Run(":3008"); err != nil {
 		log.Println("Failed to start the server:", err)
 	}
 }
