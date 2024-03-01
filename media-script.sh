8e536e95256f7bc88fc856d0a17a9539b6e08e0f
Rusted2361 <hamzasajid4040@gmail.com>
2024-03-01 15:42:23 +0500

    updated media-script.sh

--- media-script.sh
+++ media-script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#run important services
+systemctl restart ipfs.service
+systemctl restart ipfs-cluster.service
+systemctl restart s3-integration-svc
+
 # Delete all PM2 processes
 pm2 delete all
 
+#remove local changes
+git stash
+
 # Switch to main branch
 git switch main
+
+#pull recent changes
+git pull
 
 # Remove files
 rm -rf file-server-lin file-server-mac file-server-win.exe
@@ -15,8 +26,14 @@ rm -rf file-server-lin file-server-mac file-server-win.exe
 # Start PM2 process for main
 pm2 start ./file-server-lin --name Media-Server-Main-V2
 
+#remove local changes
+git stash
+
 # Switch to test-main branch
 git switch test-main
+
+#get latest changes
+git pull
 
 # Remove files
 rm -rf file-server-lin file-server-mac file-server-win.exe
@@ -27,8 +44,14 @@ rm -rf file-server-lin file-server-mac file-server-win.exe
 # Start PM2 process for staging
 pm2 start ./file-server-lin --name Media-Server-Staging-V2
 
+#remove local changes
+git stash
+
 # Switch to dev branch
 git switch dev
+
+#get latest changes
+git pull
 
 # Remove files
 rm -rf file-server-lin file-server-mac file-server-win.exe
